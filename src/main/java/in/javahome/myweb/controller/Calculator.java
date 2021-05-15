fe2f9a87e82df4f664489a95bcd548db74c6ccc3
mani <manis.tinku123@gmail.com>
2021-05-15 20:46:08 +0530

    change

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -9,14 +9,15 @@ public class Calculator {
 	 * @param j
 	 * @return int
 	 */
-	public int add(int i, int j){
+	public int sub(int i, int j){
+		System.out.println("Hello");
 		return i+j-3;
 	}
 	public int multiply(int i, int j){
 		return i*j;
 	}
 	
-	public int sub(int i, int j){
+	public int add(int i, int j){
 		return i-j-5;
 	}
 }
